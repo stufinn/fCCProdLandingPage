ca554e66cd35d827216459a6e4cc13a571bdc1c3
stufinn <30563561+finst@users.noreply.github.com>
2018-05-22 22:04:41 -0500

    Final touches

--- script.js
+++ script.js
@@ -34,3 +34,32 @@ $('a[href*="#"]')
       }
     }
   });
+
+  const targetButton = document.getElementById('targetButton'); // needs to be global scope for event listener to access
+
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  function swapReview() {
+
+    var targetElement, reviewListLength;
+    
+    var reviewList = [
+      '<h3>"Works perfectly for all my right curved bananas."<br><div id="quoteAttr">- Danielle</div></h3>',
+      '<h3>"NOT great for cereal!!"<br><div id="quoteAttr">- Thumpin&apos;</div></h3>',
+      '<h3>"Once I figured out I had to peel the banana before using - it works much better."<br><div id="quoteAttr">- Uncle Pookie</div></h3>',
+      '<h3>"It only works if you have a banana and these are not included..."<br><div id="quoteAttr">- Nadja</div></h3>',
+      '<h3>"We LOVE it!!! Looks great on the wall too!!!"<br><div id="quoteAttr">- yarvet</div></h3>',
+      '<h3>"There is a steep learning curve with this product (no pun intended). "<br><div id="quoteAttr">- Justin C.</div></h3>',
+      '<h3>"...we&apos;ve even incorporated it into our lovemaking."<br><div id="quoteAttr">- Mrs Toledo</div></h3>'];
+
+
+    reviewListLength = reviewList.length;
+    targetElement = document.getElementById('reviewContainer');
+    targetElement.innerHTML = reviewList[getRandomInt([reviewListLength])];
+  }
+
+  //listen for click on button
+  targetButton.addEventListener('click', swapReview, false);
